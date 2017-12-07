bc60ed908d60bb8f6e04366ada2a7b3ff2a41e4a
Caroline Theuri <carolinetheuriwangari2@gmail.com>
2017-12-07 08:57:38 +0300

    rendering titles to be clickable

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,2 +1,8 @@
-<h2 style="text-align:justify;color:blue;">{{$post->title}}</h2>
+<h2 style="text-align:justify;color:blue;">
+    <a href="/posts/{{$post->id}}">
+    {{$post->title}}
+</a>
+</h2>
+{{$post->created_at->toDayDateTimeString()}}
+<hr>
 <p style="text-align:justify;">{{$post->content}}</p>
